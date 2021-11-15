2794017b11bb331a79d04264e50a04b3596d9372
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-15 17:33:10 +0200

    add submenu logic

--- src/context.js
+++ src/context.js
@@ -6,7 +6,9 @@ const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({ page: '', links: [] })
 
     const openSidebar = () => {
         setIsSidebarOpen(true);
@@ -16,7 +18,10 @@ export const AppProvider = ({ children }) => {
         setIsSidebarOpen(false);
     };
 
-    const openSubmenu = () => {
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text);
+        setPage(page);
+        setLocation(coordinates);
         setIsSubmenuOpen(true);
     };
 
@@ -33,6 +38,8 @@ export const AppProvider = ({ children }) => {
                 openSidebar,
                 closeSubmenu,
                 closeSidebar,
+                location,
+                page,
             }}
         >
             {children}
